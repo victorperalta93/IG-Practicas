3d579319efa39e7ccaee839e9c2553c4c9211c82
Víctor Peralta <victorperalta.correo@gmail.com>
2019-10-07 16:12:14 +0200

    añadido esqueleto p2

---
+++ p2/objetos_B2.cc
@@ -0,0 +1,215 @@
+//**************************************************************************
+// Práctica 1 usando objetos
+//**************************************************************************
+
+#include "objetos_B2.h"
+#include "file_ply_stl.hpp"
+
+
+//*************************************************************************
+// _puntos3D
+//*************************************************************************
+
+_puntos3D::_puntos3D()
+{
+}
+
+//*************************************************************************
+// dibujar puntos
+//*************************************************************************
+
+void _puntos3D::draw_puntos(float r, float g, float b, int grosor)
+{
+int i;
+glPointSize(grosor);
+glColor3f(r,g,b);
+glBegin(GL_POINTS);
+for (i=0;i<vertices.size();i++){
+	glVertex3fv((GLfloat *) &vertices[i]);
+	}
+glEnd();
+}
+
+
+//*************************************************************************
+// _triangulos3D
+//*************************************************************************
+
+_triangulos3D::_triangulos3D()
+{
+}
+
+
+//*************************************************************************
+// dibujar en modo arista
+//*************************************************************************
+
+void _triangulos3D::draw_aristas(float r, float g, float b, int grosor)
+{
+int i;
+glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+glLineWidth(grosor);
+glColor3f(r,g,b);
+glBegin(GL_TRIANGLES);
+for (i=0;i<caras.size();i++){
+	glVertex3fv((GLfloat *) &vertices[caras[i]._0]);
+	glVertex3fv((GLfloat *) &vertices[caras[i]._1]);
+	glVertex3fv((GLfloat *) &vertices[caras[i]._2]);
+	}
+glEnd();
+}
+
+//*************************************************************************
+// dibujar en modo sólido
+//*************************************************************************
+
+void _triangulos3D::draw_solido(float r, float g, float b)
+{
+
+
+
+}
+
+//*************************************************************************
+// dibujar en modo sólido con apariencia de ajedrez
+//*************************************************************************
+
+void _triangulos3D::draw_solido_ajedrez(float r1, float g1, float b1, float r2, float g2, float b2)
+{
+
+}
+
+//*************************************************************************
+// dibujar con distintos modos
+//*************************************************************************
+
+void _triangulos3D::draw(_modo modo, float r1, float g1, float b1, float r2, float g2, float b2, float grosor)
+{
+switch (modo){
+	case POINTS:draw_puntos(r1, g1, b1, grosor);break;
+	case EDGES:draw_aristas(r1, g1, b1, grosor);break;
+	case SOLID_CHESS:draw_solido_ajedrez(r1, g1, b1, r2, g2, b2);break;
+	case SOLID:draw_solido(r1, g1, b1);break;
+	}
+}
+
+//*************************************************************************
+// clase cubo
+//*************************************************************************
+
+_cubo::_cubo(float tam)
+{
+//vertices
+
+
+// triangulos
+
+}
+
+
+//*************************************************************************
+// clase piramide
+//*************************************************************************
+
+_piramide::_piramide(float tam, float al)
+{
+
+//vertices 
+vertices.resize(5); 
+vertices[0].x=-tam;vertices[0].y=0;vertices[0].z=tam;
+vertices[1].x=tam;vertices[1].y=0;vertices[1].z=tam;
+vertices[2].x=tam;vertices[2].y=0;vertices[2].z=-tam;
+vertices[3].x=-tam;vertices[3].y=0;vertices[3].z=-tam;
+vertices[4].x=0;vertices[4].y=al;vertices[4].z=0;
+
+caras.resize(6);
+caras[0]._0=0;caras[0]._1=1;caras[0]._2=4;
+caras[1]._0=1;caras[1]._1=2;caras[1]._2=4;
+caras[2]._0=2;caras[2]._1=3;caras[2]._2=4;
+caras[3]._0=3;caras[3]._1=0;caras[3]._2=4;
+caras[4]._0=3;caras[4]._1=1;caras[4]._2=0;
+caras[5]._0=3;caras[5]._1=2;caras[5]._2=1;
+}
+
+//*************************************************************************
+// clase objeto ply
+//*************************************************************************
+
+
+_objeto_ply::_objeto_ply() 
+{
+   // leer lista de coordenadas de vértices y lista de indices de vértices
+ 
+}
+
+
+
+int _objeto_ply::parametros(char *archivo)
+{
+int n_ver,n_car;
+
+vector<float> ver_ply ;
+vector<int>   car_ply ;
+ 
+_file_ply::read(archivo, ver_ply, car_ply );
+
+n_ver=ver_ply.size()/3;
+n_car=car_ply.size()/3;
+
+printf("Number of vertices=%d\nNumber of faces=%d\n", n_ver, n_car);
+
+vertices.resize(n_ver);
+caras.resize(n_car);
+
+
+return(0);
+}
+
+
+//************************************************************************
+// objeto por revolucion
+//************************************************************************
+
+_rotacion::_rotacion()
+{
+
+}
+
+
+void _rotacion::parametros(vector<_vertex3f> perfil, int num)
+{
+int i,j;
+_vertex3f vertice_aux;
+_vertex3i cara_aux;
+int num_aux;
+
+// tratamiento de los vértice
+
+num_aux=perfil.size();
+vertices.resize(num_aux*num);
+for (j=0;j<num;j++)
+  {for (i=0;i<num_aux;i++)
+     {
+      vertice_aux.x=perfil[i].x*cos(2.0*M_PI*j/(1.0*num))+
+                    perfil[i].z*sin(2.0*M_PI*j/(1.0*num));
+      vertice_aux.z=-perfil[i].x*sin(2.0*M_PI*j/(1.0*num))+
+                    perfil[i].z*cos(2.0*M_PI*j/(1.0*num));
+      vertice_aux.y=perfil[i].y;
+      vertices[i+j*num_aux]=vertice_aux;
+     }
+  }
+
+// tratamiento de las caras 
+
+     
+ // tapa inferior
+if (fabs(perfil[0].x)>0.0)
+  {
+  }
+ 
+ // tapa superior
+ if (fabs(perfil[num_aux-1].x)>0.0)
+  {
+  }
+}
+
