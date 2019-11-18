0cd36c5c4e6179e1f676dd1c7b4f9a7f606a1f14
Víctor Peralta <victorperalta.correo@gmail.com>
2019-11-18 16:23:45 +0100

    añadida cabecera de p4

---
+++ p4/objetos_B4.h
@@ -0,0 +1,180 @@
+//**************************************************************************
+// Práctica IG usando objetos
+//**************************************************************************
+
+#include <vector>
+#include <GL/gl.h>
+#include "vertex.h"
+#include <stdlib.h>
+#include "file_ply_stl.hpp"
+
+using namespace std;
+
+const float AXIS_SIZE=5000;
+typedef enum{POINTS,EDGES,SOLID_CHESS,SOLID,SOLID_ILLUMINATED_FLAT,
+             SOLID_ILLUMINATED_GOURAUD} _modo;
+
+//*************************************************************************
+// clase punto
+//*************************************************************************
+
+class _puntos3D
+{
+public:
+
+  
+	_puntos3D();
+void 	draw_puntos(float r, float g, float b, int grosor);
+
+vector<_vertex3f> vertices;
+};
+
+//*************************************************************************
+// clase triángulo
+//*************************************************************************
+
+class _triangulos3D: public _puntos3D
+{
+public:
+
+	_triangulos3D();
+void 	draw_aristas(float r, float g, float b, int grosor);
+void    draw_solido(float r, float g, float b);
+void 	draw_solido_ajedrez(float r1, float g1, float b1, float r2, float g2, float b2);
+void 	draw_iluminacion_plana( );
+void 	draw_iluminacion_suave( );
+
+void 	draw(_modo modo, float r1, float g1, float b1, float r2, float g2, float b2, float grosor);
+
+void	calcular_normales_caras();
+void 	calcular_normales_vertices();
+
+vector<_vertex3i> caras;
+
+vector<_vertex3f> normales_caras;
+vector<_vertex3f> normales_vertices;
+
+bool b_normales_caras;
+bool b_normales_vertices;
+
+_vertex4f ambiente_difusa;     //coeficientes ambiente y difuso
+_vertex4f especular;           //coeficiente especular
+float brillo;                  //exponente del brillo 
+
+};
+
+
+//*************************************************************************
+// clase cubo
+//*************************************************************************
+
+class _cubo: public _triangulos3D
+{
+public:
+
+	_cubo(float tam=0.5);
+};
+
+
+//*************************************************************************
+// clase piramide
+//*************************************************************************
+
+class _piramide: public _triangulos3D
+{
+public:
+
+	_piramide(float tam=0.5, float al=0.75);
+};
+
+//*************************************************************************
+// clase objeto ply
+//*************************************************************************
+
+class _objeto_ply: public _triangulos3D
+{
+public:
+   _objeto_ply();
+
+int   parametros(char *archivo);
+};
+
+//************************************************************************
+// objeto por revolución
+//************************************************************************
+
+class _rotacion: public _triangulos3D
+{
+public:
+       _rotacion();
+void  parametros(vector<_vertex3f> perfil1, int num1, int tapas);
+
+vector<_vertex3f> perfil; 
+int num;
+};
+
+
+//************************************************************************
+// objeto articulado: tanque
+//************************************************************************
+
+class _chasis: public _triangulos3D
+{
+public:
+       _chasis();
+void 	draw(_modo modo, float r1, float g1, float b1, float r2, float g2, float b2, float grosor);
+
+float altura;
+
+protected:
+_rotacion  rodamiento;
+_cubo  base;
+};
+
+//************************************************************************
+
+class _torreta: public _triangulos3D
+{
+public:
+       _torreta();
+void 	draw(_modo modo, float r1, float g1, float b1, float r2, float g2, float b2, float grosor);
+
+float altura;
+float anchura;
+
+protected:
+_cubo  base;
+_piramide parte_trasera;
+};
+
+//************************************************************************
+
+class _tubo: public _triangulos3D
+{
+public:
+       _tubo();
+void 	draw(_modo modo, float r1, float g1, float b1, float r2, float g2, float b2, float grosor);
+
+protected:
+_rotacion tubo_abierto; // caña del cañón
+};
+
+//************************************************************************
+
+class _tanque: public _triangulos3D
+{
+public:
+       _tanque();
+void 	draw(_modo modo, float r1, float g1, float b1, float r2, float g2, float b2, float grosor);
+
+float giro_tubo;
+float giro_torreta;
+
+float giro_tubo_min;
+float giro_tubo_max;
+
+protected:
+_chasis  chasis;
+_torreta  torreta;
+_tubo     tubo;
+};
